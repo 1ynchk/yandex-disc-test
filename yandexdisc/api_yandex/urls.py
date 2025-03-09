a661ae7b64d0befe5a4824f8d1df315dfd6cd35f
1ynchk <saspery@yandex.ru>
2025-03-09 19:05:14 +0300

    get download link view is finished

--- yandexdisc/api_yandex/urls.py
+++ yandexdisc/api_yandex/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import get_yandex_files
+from .views import get_yandex_files, get_yandex_download_link
 
 urlpatterns = [
-    path('get_files/', get_yandex_files)    
+    path('get_files/', get_yandex_files),
+    path('get_file_link/', get_yandex_download_link)
 ]
--- yandexdisc/api_yandex/views.py
+++ yandexdisc/api_yandex/views.py
@@ -1,10 +1,16 @@
 import requests
+import os
+import dotenv
+import urllib.parse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-YANDEX_API_BASE = "https://cloud-api.yandex.net/v1/disk/public/resources"
+dotenv.load_dotenv()
 
-@api_view(["GET"])
+YANDEX_API_BASE_GET_LIST = 'https://cloud-api.yandex.net/v1/disk/public/resources'
+YANDEX_API_BASE_GET_LINK = 'https://cloud-api.yandex.net/v1/disk/public/resources/download'
+
+@api_view(['GET'])
 def get_yandex_files(request):
     '''Получение файлов с публичной ссылки'''
     
@@ -18,9 +24,9 @@ def get_yandex_files(request):
             }, 
             status=400
             )
-
+    headers =  {'Authorization': f'OAuth {os.getenv('YANDEX_OAUTH_TOKEN')}'}
     params = {'public_key': public_key}
-    response = requests.get(YANDEX_API_BASE, params=params)
+    response = requests.get(YANDEX_API_BASE_GET_LIST, params=params, headers=headers)
 
     if response.status_code == 200:
         return Response(response.json())
@@ -32,3 +38,29 @@ def get_yandex_files(request):
             }, 
             status=response.status_code
         )
+
+@api_view(['GET'])
+def get_yandex_download_link(request):
+    """Получение ссылки для скачивания публичного файла с Яндекс.Диска."""
+    
+    file_path = request.GET.get('path')
+    public_key = request.GET.get('public_key')
+    
+    if not file_path or not public_key:
+        return Response({'error': 'Укажите path и public_key в параметрах запроса'}, status=400)
+
+    encoded_path = urllib.parse.unquote_plus(file_path)  
+    encoded_key = urllib.parse.quote_plus(public_key)
+    
+    params = {
+        'path': encoded_path,
+        'public_key': encoded_key
+    }
+
+    response = requests.get(YANDEX_API_BASE_GET_LINK, params=params)
+    data = response.json()
+
+    if response.status_code == 200 and 'href' in data:
+        return Response({'download_url': data['href'], 'name': file_path.replace('/', '')})
+    else:
+        return Response(data, status=response.status_code)
